225718a6aa0001efad3fbed99fadb42621291a58
Dimitris Porichis <porichisdimitris@gmail.com>
2025-03-05 15:32:17 +0200

    selecting target files for analysis

--- demo.py
+++ demo.py
@@ -47,9 +47,57 @@ def extract_firmware(file_path):
                 #         result = subprocess.run(["objdump", "-D", f"{extract_path}/{hex_string}/{name}"], stdout=d, check=True)
 
 
+def scan_ports(json_path):
+
+    with open(json_path, "r") as file:
+        data = json.load(file)
+    print(data)
+
+    elf_files = []
+    config_files = []
+
+    for item in data:
+        # If it is a final text file
+        if len(item["Analysis"]["file_map"]) == 0:
+            # Check if it has a conf extension
+            if(item["Analysis"]["file_path"].endswith(".conf")):
+                config_files.append(item["Analysis"]["file_path"])
+        # For files with relevant desc
+        elif len(item["Analysis"]["file_map"]) == 1:
+            # Keep all elf files for analysis
+            if(item["Analysis"]["file_map"][0]["name"] == "elf"):
+                elf_files.append(item["Analysis"]["file_path"])
+
+    print(len(elf_files))
+    print(len(config_files))
+
+
+    # Perform scan of relevant function calls on all elf_files
+    target_elfs = []
+    for file in elf_files:
+        try:
+            result = subprocess.run(['strings', file], capture_output=True, text=True)
+            if any(word in result.stdout for word in ['bind', 'listen', 'accept', 'socket']):
+                target_elfs.append(file)
+        except Exception as e:
+            print(f"Re auto xalase {file}: {e}")
+
+    # Number of network related ELFs
+    print(len(target_elfs))
+
+    return
+
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run binwalk on a given binary file.")
     parser.add_argument("file", help="Path to the binary file")
     args = parser.parse_args()
     
     extract_firmware(args.file)
+    export_json = f"./temp_{os.path.basename(args.file)}/structure.json"
+    print(export_json)
+
+    scan_ports(export_json)
+
+
